dfac4ee54662beb6147f45fc3dc67c0e82fbf664
NICOLLAS PROTTI DA FONSECA <05548523066@senacrs.edu.br>
2026-05-26 17:02:13 -0300

    fizemos o historia/form.js

---
+++ app/scripts/form.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('post-form')
+
+form.addEventListener('submit', function(event) {
+
+    event.preventDefault();
+
+    const form_data = new FormData(event.target)
+
+    const autor = form_data.get('autor')
+    const titulo = form_data.get('titulo')
+    const email = form_data.get('email')
+    const historia = form_data.get('historia')
+
+    if (!autor || !historia) { //verificação de campos vazios
+        alert("Os campos `Autor` e `História` são obrigatórios!")
+        return
+    }
+
+    const article = document.createElement('article');
+    article.className = "artigo";
+
+    article.innerHTML =
+        `<h3>${titulo}</h3>
+      <p><strong> Autor: ${autor}</strong></P>
+      <p><small> Email: ${email}</small></p>
+      <p>${historia.replace('/\n/g', '<br />')}</p>
+      <hr />`;
+
+      document.getElementById('historias').appendChild(article)
+
+      event.target.reset()
+
+})
