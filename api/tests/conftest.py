7d405f199a2aa1de911c250d9d929b82b83f5e19
NICOLLAS PROTTI DA FONSECA <05548523066@senacrs.edu.br>
2026-06-26 16:53:51 -0300

    models

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from viajei_api.models import table_registry
 
 from viajei_api.app import app
 
@@ -7,3 +10,15 @@ from viajei_api.app import app
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine("sqlite:///:memory:")
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+    engine.dispose()
